8a70efad347367b9f42f9a41b4917c0643cd7b8e
Mathias_Möll <144792835+MathMoell@users.noreply.github.com>
2025-06-09 15:08:00 +0300

    Delete sport directory

--- sport/config.php
+++
@@ -1,8 +0,0 @@
-<?php
-// Ühendus SQLite andmebaasiga
-try {
-    $yhendus = new SQLite3('sport.db');  // Veendu, et sport.db asub samas kaustas
-} catch (Exception $e) {
-    die("Andmebaasi viga: " . $e->getMessage());
-}
-?>
